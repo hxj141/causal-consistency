90400f3c7b887096fa5536e138dce06591975ffe
hxj141 <hxj141@psu.edu>
2022-04-14 16:27:09 -0400

    Continued work

--- client.py
+++ client.py
@@ -6,10 +6,10 @@ import time
 identity = input("Are you Alice (A) or Bob (B)?")
 if identity == "A": # Default Alice to West datacenter
     name = "A"
-    port = 8000 
+    port = 7990 
 if identity == "B": # Default Bob to Central datacenter
     name = "B"
-    port = 8001 
+    port = 7991 
 
 clock = 0 # This clock will keep track of what is going on
 
@@ -18,34 +18,33 @@ s.connect(('127.0.0.1', port)) #Port 8000 is datacenter 1, Port 8001 is datacent
 
 while True: 
     s.send(name.encode()) # Inform server of identity
-    srv_id = s.recv(1024).decode() # Find out server identity
+    srv_id = int(s.recv(1024).decode()) # Find out server identity
     # Alice logic
     if name == "A":
         while True: 
             command = input("What would you like to send?")
-                if command == "lost": # Write "lost" to DC1
-                    data = pickle.dumps(["W_LOST", [srv_id, clock]]) # Message being sent, [datacenter ID, timestamp]
-                    s.send(data)
-                if command == "found": # Write "found" to DC1
-                    # Read first to check if DC1 has lost 
-                    data = pickle.dumps(["W_FOUND", [srv_id, clock]]) # Message being sent, [datacenter ID, timestamp]
-                    s.send(data)
-                if command == "wait": # Wait for t seconds and adjust clock accordingly
-                    t = int(input("How many seconds would you like to wait?"))
-                    time.sleep(t) 
-                    clock += t       
+            if command == "lost": # Write "lost" to DC1
+                data = pickle.dumps(["W_LOST", srv_id]) # Message being sent, [datacenter ID, timestamp]
+                s.send(data)
+            if command == "found": # Write "found" to DC1
+                # Read first to check if DC1 has lost 
+                data = pickle.dumps(["W_FOUND", srv_id]) # Message being sent, [datacenter ID, timestamp]
+                s.send(data)
+            if command == "wait": # Wait for t seconds and adjust clock accordingly
+                t = int(input("How many seconds would you like to wait?"))
+                time.sleep(t) 
+                clock += t       
     # Bob logic
     if name == "B":
         while True: 
             command = input("What would you like to do?")
-                
-                if command == "glad": # Write "found" to DC1
-                    # Read first to check if DC2 has found
-                    data = pickle.dumps(["W_GLAD", [srv_id, clock]]) # Message being sent, [datacenter ID, timestamp]
-                    s.send(data)
-                if command == "wait": # Wait for t seconds and adjust clock accordingly
-                    t = int(input("How many seconds would you like to wait?"))
-                    time.sleep(t) 
-                    clock += t        
+            if command == "glad": # Write "glad" to DC2
+                # Read first to check if DC2 has foundN
+                data = pickle.dumps(["W_GLAD", srv_id]) # Message being sent, [datacenter ID, timestamp]
+                s.send(data)
+            if command == "wait": # Wait for t seconds and adjust clock accordingly
+                t = int(input("How many seconds would you like to wait?"))
+                time.sleep(t) 
+                clock += t        
 
 #Alice: I've lost my wedding ring."  "Bob: I'm glad to hear that."
--- server.py
+++ server.py
@@ -1,23 +1,44 @@
 import socket 
 import threading 
 import time
+import pickle
 
-clock = 0; # Set up a clock to keep track of 
+# REWRITE ENTIRE CODE TO MAKE IT SO ANY SERVER CAN SEND TO ANY OTHER 
 
-loc = input("Which data center is this (1, 2, or 3)?")
-if loc == 1:
+# have everything use one thread for connecting, send a message on connection to identify either client or peer, logic from there
+# have the other thread use the other servers' ports
+
+# Set up global variables
+status = []  # Message to pass data between threads
+clock = 0  # Lamport clock to handle timestamps
+dependency = {}  # Dependency data for the server, used as main reference for consistency
+loc = int(input("Which data center is this (1, 2, or 3)?"))  # Server ID, user input
+
+# Depending on where the server is, set port and recieve ports accordingly
+if loc == 1: 
     port = 8000
+    peer_port = [8101,8102]
 if loc == 2:
-    port = 8001
+    port = 8001 
+    peer_port = [8100,8102]
 if loc == 3:
     port = 8002
-dependency = {} # List of connected peers
-s = socket.socket() 
-s.bind(('127.0.0.1', port)) 
-s.listen() 
-print("Waiting for clients...") # The message which displays before peers connect
+    peer_port = [8100,8101]
+
+srv_port = port + 100
+
+# Prepare sockets
+srv_sock = socket.socket() # Socket for inter-server connection (server-like function)
+rpl_sock = socket.socket() # Socket for inter-server connection (client-like function)
+
+cli_sock = socket.socket() # Socket for client connection
+cli_sock.bind(('127.0.0.1', port))
+cli_sock.listen()
+
+print("Loading...") # The message which displays before client connection
 
 
+# Thread which handles client connections to the server
 def client_target(cli, client):
     # Get client identity
     data = cli.recv(1024)
@@ -27,25 +48,32 @@ def client_target(cli, client):
         name = "Bob"
     print(name + " has connected at " + client)
     # Send over datacenter ID
-    data = loc.encode()
-    cli.send(data) 
+    cli.send(str(loc).encode()) 
     while True:
         data = cli.recv(1024)
         data = pickle.loads(data)
-        op_signal = pickle.loads(data)[1]
+        op_signal = pickle.loads(data)[0]
         if op_signal[0:2] == "W_":
             if op_signal[2:] == "LOST":
-                # update dependency
-                # prop to others
-                #might not even need all these ifs, just pass the slice as parameter
-                pass
+                clock += 1
+                dependency[client].append([op_signal[2:], [loc, clock]])
+                status = [op_signal[2:], [loc, clock]]
+                print("Alice: I have lost my wedding ring!")
             if op_signal[2:] == "FOUND":
-                pass
+                if "LOST" in dependency[client]:
+                    clock += 1
+                    dependency[client].append([op_signal[2:], [loc, clock]])
+                    status = [op_signal[2:], [loc, clock]]
+                    print("Alice: I have found my wedding ring!")
             if op_signal[2:] == "GLAD":
-                pass
-        if op_signal[0:2] == "R_"       
-            if op_signal[2:] == "LOST":
-                pass
+                for d in dependency: 
+                    if dependency[d][0] == "FOUND": # If FOUND appears in any of the dependency entries
+                        clock += 1
+                        dependency[client].append([op_signal[2:], [loc, clock]])
+                        status = [op_signal[2:], [loc, clock]]
+                        print("Bob: I am glad to hear that!")
+                        break
+        if op_signal[0:2] == "R_":       
             if op_signal[2:] == "FOUND":
                 pass
             if op_signal[2:] == "GLAD":
@@ -54,8 +82,53 @@ def client_target(cli, client):
             break 
         cli.send(data)
 
-while True: 
-    cli, address = s.accept()
+# Thread for every server-type connection to other servers
+def primary_target():
+    # Set up server
+    srv_sock.bind(('127.0.0.1', srv_port))
+    srv_sock.listen()
+    srv, address = srv_sock.accept()
+     # Wait to recieve location info, so you can adjust delays accordingly
+    data = srv.recv(1024)
+    peer_loc = int(data.decode())
+    while True:
+        # When status is set, get ready to send it to other datacenters 
+        if len(status) != 0:
+            # Simulate a delay which scales with distance
+            delay = abs(peer_loc-loc)*2 
+            time.sleep(delay)
+            # Send data over to requesting server
+            data = pickle.dumps(status)
+            srv.send(data)
+            
+            
+# Thread for every client-type connection to other servers
+def replica_target(in_port):
+    # Connect to corresponding server, inform them of your location
+    rpl_sock.connect(('127.0.0.1', in_port))
+    rpl_sock.send(str(loc).encode())
+    while True:
+    # Copy infomation from message into dependency list; if more messages exist, add another thing to the message to handle cases
+        data = rpl_sock.recv(1024)
+        dep_info = pickle.loads(data)
+        dependency[client].append([dep_info[0], [dep_info[1][0], dep_info[1][1]]])
+        # Print message once recieved
+        if dep_info[0] == "LOST":
+            print("Alice: I have lost my wedding ring!")
+        if dep_info[0] == "FOUND":
+            print("Alice: I have found my wedding ring!")
+        if dep_info[0] == "GLAD":
+            print("Bob: I am glad to hear that!")
+
+while True:
+    serverConnect = threading.Thread(target=primary_target, args=()).start()
+    cli, address = cli_sock.accept()
     client = str(address[0])+":"+str(address[1])
     dependency[client] = [] # Add new connection to dependencies list
-    threading.Thread(target=client_target, args=(cli,client)).start()
+    print("Ready!")
+    # Get the threads running: one for serving client, one for serving the others, one per each of the others' servers
+    clientConnect = threading.Thread(target=client_target, args=(cli,client)).start()
+    for p in peer_port:
+        print(p,peer_port) 
+        replicaConnect = threading.Thread(target=replica_target, args=(p,)).start()
+            
